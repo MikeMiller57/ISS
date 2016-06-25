e7a9fd7ef7a7c6d7527bc60f0286252891f53617
jfishpants@yahoo.com <mikemandm222@gmail.com>
2016-06-25 16:40:29 -0400

    added call to wheretheissat api. handeled in asyncTask in new fragment 'ISSInfoFragment. posts data to listView.

--- app/src/main/java/com/example/glados/iss/MainActivity.java
+++ app/src/main/java/com/example/glados/iss/MainActivity.java
@@ -8,10 +8,17 @@ import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new ISSInfoFragment())
+                    .commit();
+        }
     }
 
     @Override
